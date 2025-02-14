5771f1369f2890c91366e36ece72164bf43fa604
[!] BrunoO <38006634+barbaops@users.noreply.github.com>
2025-02-13 22:43:52 -0300

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "vpc_id" {
+  description = "ID da VPC criada"
+  value       = aws_vpc.this.id
+}
+
+output "internet_gateway_id" {
+  description = "ID do Internet Gateway"
+  value       = aws_internet_gateway.this.id
+}
