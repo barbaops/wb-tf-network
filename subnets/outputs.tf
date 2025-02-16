ce2bb666c31972f3dc4ad8e1725f4f2017b910cc
[!] BrunoO <38006634+barbaops@users.noreply.github.com>
2025-02-16 14:53:52 +0000

    fix - subnet export cidrs to nacl

--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -2,3 +2,8 @@ output "subnet_ids" {
   description = "IDs das subnets criadas"
   value       = aws_subnet.this[*].id
 }
+
+output "private_subnet_cidrs" {
+  description = "Lista de CIDRs das subnets privadas"
+  value       = aws_subnet.private[*].cidr_block
+}
