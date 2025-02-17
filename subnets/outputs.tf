b418aede67bd1b1734fa16477d481ed0255098c7
[!] BrunoO <38006634+barbaops@users.noreply.github.com>
2025-02-17 14:12:36 -0300

    feat - create internet gateway and routetable

--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -1,9 +1,14 @@
+output "internet_gateway_id" {
+  description = "ID do Internet Gateway (se criado)"
+  value       = length(aws_internet_gateway.this) > 0 ? aws_internet_gateway.this[0].id : null
+}
+
+output "public_route_table_id" {
+  description = "ID da Tabela de Rotas Pública (se criada)"
+  value       = length(aws_route_table.public) > 0 ? aws_route_table.public[0].id : null
+}
+
 output "subnet_ids" {
   description = "IDs das subnets criadas"
   value       = aws_subnet.this[*].id
-}
-
-output "private_subnet_cidrs" {
-  description = "Lista de CIDRs das subnets privadas"
-  value       = [for subnet in aws_subnet.this : subnet.cidr_block if lookup(subnet.tags, "Type", "private") == "private"]
 }
