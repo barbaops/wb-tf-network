55232dab6607321733dd20091953e4e53b97f12f
[!] BrunoO <38006634+barbaops@users.noreply.github.com>
2025-02-17 12:52:17 -0300

    feat - VPC output name

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -7,3 +7,8 @@ output "internet_gateway_id" {
   description = "ID do Internet Gateway"
   value       = aws_internet_gateway.this.id
 }
+
+output "vpc_name" {
+  description = "Nome da VPC"
+  value       = aws_vpc.this.tags["Name"]
+}
