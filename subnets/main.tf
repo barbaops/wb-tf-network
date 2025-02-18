06d37c45167f21cfe65d409da37c94a0320f8729
[!] BrunoO <38006634+barbaops@users.noreply.github.com>
2025-02-18 19:35:57 -0300

    Update main.tf

--- subnets/main.tf
+++ subnets/main.tf
@@ -1,62 +1,61 @@
 resource "aws_subnet" "this" {
-  for_each = { for subnet in var.subnets : subnet.name => subnet }
+  for_each = { for s in var.subnets : s.name => s }
 
-  vpc_id                  = var.vpc_id
-  cidr_block              = each.value.cidr
-  availability_zone       = each.value.az
-  map_public_ip_on_launch = lookup(each.value, "public", false)
+  vpc_id            = var.vpc_id
+  cidr_block        = each.value.cidr
+  availability_zone = each.value.az
+  map_public_ip_on_launch = each.value.type == "public" ? true : false
 
   tags = merge(var.tags, {
-    "Name" = each.value.name
+    "Name" = "${var.vpc_name}-${each.value.name}"
+    "Type" = each.value.type
   })
 }
 
+resource "aws_internet_gateway" "this" {
+  count  = length([for s in var.subnets : s if s.type == "public"]) > 0 ? 1 : 0
+  vpc_id = var.vpc_id
+
+  tags = merge(var.tags, { "Name" = "${var.vpc_name}-igw" })
+}
+
 resource "aws_eip" "nat" {
-  count  = var.create_nat_gateway ? 1 : 0
+  count  = length([for s in var.subnets : s if s.type == "private"]) > 0 ? 1 : 0
   domain = "vpc"
 }
 
 resource "aws_nat_gateway" "this" {
-  count         = var.create_nat_gateway ? 1 : 0
+  count         = length([for s in var.subnets : s if s.type == "private"]) > 0 ? 1 : 0
   allocation_id = aws_eip.nat[0].id
-  subnet_id     = aws_subnet.this[var.nat_subnet].id
+  subnet_id     = aws_subnet.this["public-1a"].id
 
-  tags = merge(var.tags, {
-    "Name" = "nat-gateway"
-  })
+  tags = merge(var.tags, { "Name" = "${var.vpc_name}-nat" })
 }
 
-resource "aws_route_table" "this" {
-  for_each = { for subnet in var.subnets : subnet.name => subnet if subnet.type == "private" }
-
+resource "aws_route_table" "public" {
+  count  = length([for s in var.subnets : s if s.type == "public"]) > 0 ? 1 : 0
   vpc_id = var.vpc_id
 
-  tags = merge(var.tags, {
-    "Name" = "${each.key}-rt"
-  })
+  tags = merge(var.tags, { "Name" = "${var.vpc_name}-public-rt" })
 }
 
-resource "aws_route" "nat" {
-  for_each = aws_route_table.this
+resource "aws_route" "public" {
+  count          = length(aws_route_table.public) > 0 ? 1 : 0
+  route_table_id = aws_route_table.public[0].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this[0].id
+}
 
-  route_table_id         = each.value.id
+resource "aws_route_table" "private" {
+  count  = length([for s in var.subnets : s if s.type == "private"]) > 0 ? 1 : 0
+  vpc_id = var.vpc_id
+
+  tags = merge(var.tags, { "Name" = "${var.vpc_name}-private-rt" })
+}
+
+resource "aws_route" "private" {
+  count          = length(aws_route_table.private) > 0 ? 1 : 0
+  route_table_id = aws_route_table.private[0].id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_nat_gateway.this[0].id
 }
-
-resource "aws_route_table_association" "this" {
-  for_each = aws_route_table.this
-
-  subnet_id      = aws_subnet.this[each.key].id
-  route_table_id = each.value.id
-}
-
-resource "aws_internet_gateway" "this" {
-  count  = length([for subnet in var.subnets : subnet if subnet.public]) > 0 ? 1 : 0
-  vpc_id = var.vpc_id
-
-  tags = merge(var.tags, {
-    "Name" = "internet-gateway"
-  })
-}
-
