be29e819f5daf9dceeb6fa2aa497c0ff9879d789
[!] BrunoO <38006634+barbaops@users.noreply.github.com>
2025-02-18 19:34:55 -0300

    Update variables.tf

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,15 +1,20 @@
-variable "vpc_cidr" {
-  description = "Bloco CIDR da VPC"
-  type        = string
-}
-
 variable "vpc_name" {
   description = "Nome da VPC"
   type        = string
 }
 
+variable "cidr_block" {
+  description = "CIDR da VPC"
+  type        = string
+}
+
+variable "additional_cidr_block" {
+  description = "CIDR adicional para a VPC (opcional)"
+  type        = string
+  default     = null
+}
+
 variable "tags" {
-  description = "Tags a serem aplicadas à VPC"
+  description = "Tags a serem aplicadas na VPC"
   type        = map(string)
-  default     = {}
 }
