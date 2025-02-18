d6b907b5d0bf4f929de206248299bd818a5846ca
[!] BrunoO <38006634+barbaops@users.noreply.github.com>
2025-02-18 19:55:59 -0300

    Update outputs.tf

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -8,6 +8,11 @@ output "vpc_cidr" {
   value       = aws_vpc.this.cidr_block
 }
 
+output "vpc_name" {
+  description = "Nome da VPC"
+  value       = aws_vpc.this.tags["Name"]
+}
+
 output "vpc_additional_cidr" {
   description = "CIDR adicional (se configurado)"
   value       = length(aws_vpc_ipv4_cidr_block_association.additional) > 0 ? aws_vpc_ipv4_cidr_block_association.additional[0].cidr_block : null
