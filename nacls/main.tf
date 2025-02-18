f80fc1acc27a1dab99bd30b3e39552f5b0112f28
[!] BrunoO <38006634+barbaops@users.noreply.github.com>
2025-02-18 19:37:22 -0300

    Update main.tf

--- nacls/main.tf
+++ nacls/main.tf
@@ -1,17 +1,45 @@
 resource "aws_network_acl" "this" {
+  for_each = { for nacl in var.nacls : nacl.name => nacl }
+
   vpc_id = var.vpc_id
-  tags   = merge(var.tags, { "Name" = "${var.nacl_name}" })
+
+  tags = merge(var.tags, {
+    "Name" = "${var.vpc_name}-${each.value.name}-nacl"
+  })
 }
 
+# 🔹 Criando regras de entrada
 resource "aws_network_acl_rule" "ingress" {
-  count = length(var.ingress_rules)
+  for_each = { for nacl in var.nacls : nacl.name => nacl if length(nacl.ingress_rules) > 0 }
 
-  network_acl_id = aws_network_acl.this.id
-  rule_number    = var.ingress_rules[count.index]["rule_number"]
-  protocol       = var.ingress_rules[count.index]["protocol"]
-  rule_action    = var.ingress_rules[count.index]["rule_action"]
-  cidr_block     = var.ingress_rules[count.index]["cidr_block"]
-  from_port      = var.ingress_rules[count.index]["from_port"]
-  to_port        = var.ingress_rules[count.index]["to_port"]
+  network_acl_id = aws_network_acl.this[each.key].id
+  rule_number    = each.value.ingress_rules[0].rule_number
+  protocol       = each.value.ingress_rules[0].protocol
+  action         = each.value.ingress_rules[0].action
+  cidr_block     = each.value.ingress_rules[0].cidr_block
+  from_port      = each.value.ingress_rules[0].from_port
+  to_port        = each.value.ingress_rules[0].to_port
   egress         = false
+}
+
+# 🔹 Criando regras de saída
+resource "aws_network_acl_rule" "egress" {
+  for_each = { for nacl in var.nacls : nacl.name => nacl if length(nacl.egress_rules) > 0 }
+
+  network_acl_id = aws_network_acl.this[each.key].id
+  rule_number    = each.value.egress_rules[0].rule_number
+  protocol       = each.value.egress_rules[0].protocol
+  action         = each.value.egress_rules[0].action
+  cidr_block     = each.value.egress_rules[0].cidr_block
+  from_port      = each.value.egress_rules[0].from_port
+  to_port        = each.value.egress_rules[0].to_port
+  egress         = true
+}
+
+# 🔹 Associando as NACLs às Subnets
+resource "aws_network_acl_association" "this" {
+  for_each = { for nacl in var.nacls : nacl.name => nacl if length(nacl.subnet_ids) > 0 }
+
+  network_acl_id = aws_network_acl.this[each.key].id
+  subnet_id      = each.value.subnet_ids[0]
 }
