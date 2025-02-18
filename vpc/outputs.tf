739c8021d0a6971ef6b6f26bb8d6765510fcc3b4
[!] BrunoO <38006634+barbaops@users.noreply.github.com>
2025-02-18 19:35:20 -0300

    Update outputs.tf

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -3,12 +3,12 @@ output "vpc_id" {
   value       = aws_vpc.this.id
 }
 
-output "internet_gateway_id" {
-  description = "ID do Internet Gateway"
-  value       = aws_internet_gateway.this.id
+output "vpc_cidr" {
+  description = "CIDR principal da VPC"
+  value       = aws_vpc.this.cidr_block
 }
 
-output "vpc_name" {
-  description = "Nome da VPC"
-  value       = aws_vpc.this.tags["Name"]
+output "vpc_additional_cidr" {
+  description = "CIDR adicional (se configurado)"
+  value       = length(aws_vpc_ipv4_cidr_block_association.additional) > 0 ? aws_vpc_ipv4_cidr_block_association.additional[0].cidr_block : null
 }
