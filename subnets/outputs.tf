382e14b0997a6839ab9f6e9912b8398c3cd51c37
[!] BrunoO <38006634+barbaops@users.noreply.github.com>
2025-02-18 19:37:08 -0300

    Update outputs.tf

--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -1,3 +1,18 @@
+output "public_subnet_ids" {
+  description = "IDs das subnets públicas"
+  value       = { for s in aws_subnet.this : s.tags.Name => s.id if s.map_public_ip_on_launch }
+}
+
+output "private_subnet_ids" {
+  description = "IDs das subnets privadas"
+  value       = { for s in aws_subnet.this : s.tags.Name => s.id if s.tags.Type == "private" }
+}
+
+output "database_subnet_ids" {
+  description = "IDs das subnets de banco de dados"
+  value       = { for s in aws_subnet.this : s.tags.Name => s.id if s.tags.Type == "database" }
+}
+
 output "internet_gateway_id" {
   description = "ID do Internet Gateway (se criado)"
   value       = length(aws_internet_gateway.this) > 0 ? aws_internet_gateway.this[0].id : null
@@ -6,14 +21,4 @@ output "internet_gateway_id" {
 output "nat_gateway_id" {
   description = "ID do NAT Gateway (se criado)"
   value       = length(aws_nat_gateway.this) > 0 ? aws_nat_gateway.this[0].id : null
-}
-
-output "public_subnet_ids" {
-  description = "IDs das subnets públicas criadas"
-  value       = { for subnet in aws_subnet.this : subnet.tags.Name => subnet.id if subnet.map_public_ip_on_launch }
-}
-
-output "private_subnet_ids" {
-  description = "IDs das subnets privadas criadas"
-  value       = { for subnet in aws_subnet.this : subnet.tags.Name => subnet.id if !subnet.map_public_ip_on_launch }
 }
