6fc9a9eb82f0858bb870ae9cbe9d68053f35d3f0
[!] BrunoO <38006634+barbaops@users.noreply.github.com>
2025-02-18 18:02:15 -0300

    Update outputs.tf

--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -1,21 +1,11 @@
-output "vpc_id" {
-  description = "ID da VPC criada"
-  value       = var.vpc_id
-}
-
 output "internet_gateway_id" {
-  description = "ID do Internet Gateway criado (se existir)"
+  description = "ID do Internet Gateway (se criado)"
   value       = length(aws_internet_gateway.this) > 0 ? aws_internet_gateway.this[0].id : null
 }
 
 output "nat_gateway_id" {
   description = "ID do NAT Gateway (se criado)"
   value       = length(aws_nat_gateway.this) > 0 ? aws_nat_gateway.this[0].id : null
-}
-
-output "public_route_table_id" {
-  description = "ID da Route Table pública"
-  value       = length(aws_route_table.this) > 0 ? aws_route_table.this[0].id : null
 }
 
 output "public_subnet_ids" {
@@ -26,19 +16,4 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   description = "IDs das subnets privadas criadas"
   value       = { for subnet in aws_subnet.this : subnet.tags.Name => subnet.id if !subnet.map_public_ip_on_launch }
-}
-
-output "database_subnet_ids" {
-  description = "IDs das subnets de banco de dados criadas"
-  value       = { for subnet in aws_subnet.this : subnet.tags.Name => subnet.id if subnet.tags.Type == "database" }
-}
-
-output "pods_subnet_ids" {
-  description = "IDs das subnets para Pods (EKS) criadas"
-  value       = { for subnet in aws_subnet.this : subnet.tags.Name => subnet.id if subnet.tags.Type == "pods" }
-}
-
-output "subnet_cidrs" {
-  description = "Lista de CIDRs de todas as subnets criadas"
-  value       = { for subnet in aws_subnet.this : subnet.tags.Name => subnet.cidr_block }
 }
