728bc01e96d838019a635c942861a6a4c2d08854
[!] BrunoO <38006634+barbaops@users.noreply.github.com>
2025-02-18 20:15:04 -0300

    Update outputs.tf

--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -1,24 +1,24 @@
 output "public_subnet_ids" {
-  description = "IDs das subnets públicas"
-  value       = { for s in aws_subnet.this : s.tags.Name => s.id if s.map_public_ip_on_launch }
+  description = "IDs das subnets públicas criadas"
+  value       = { for k, v in aws_subnet.this : k => v.id if v.map_public_ip_on_launch }
 }
 
 output "private_subnet_ids" {
-  description = "IDs das subnets privadas"
-  value       = { for s in aws_subnet.this : s.tags.Name => s.id if s.tags.Type == "private" }
-}
-
-output "database_subnet_ids" {
-  description = "IDs das subnets de banco de dados"
-  value       = { for s in aws_subnet.this : s.tags.Name => s.id if s.tags.Type == "database" }
-}
-
-output "internet_gateway_id" {
-  description = "ID do Internet Gateway (se criado)"
-  value       = length(aws_internet_gateway.this) > 0 ? aws_internet_gateway.this[0].id : null
+  description = "IDs das subnets privadas criadas"
+  value       = { for k, v in aws_subnet.this : k => v.id if !v.map_public_ip_on_launch }
 }
 
 output "nat_gateway_id" {
-  description = "ID do NAT Gateway (se criado)"
+  description = "ID do NAT Gateway criado (se aplicável)"
   value       = length(aws_nat_gateway.this) > 0 ? aws_nat_gateway.this[0].id : null
+}
+
+output "public_route_table_id" {
+  description = "ID da Route Table pública"
+  value       = length(aws_route_table.public) > 0 ? aws_route_table.public[0].id : null
+}
+
+output "private_route_table_id" {
+  description = "ID da Route Table privada"
+  value       = length(aws_route_table.private) > 0 ? aws_route_table.private[0].id : null
 }
