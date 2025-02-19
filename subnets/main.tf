93dbeb7bdb547a969b68e03706e2a39403023a72
[!] BrunoO <38006634+barbaops@users.noreply.github.com>
2025-02-18 21:06:47 -0300

    Update main.tf

--- subnets/main.tf
+++ subnets/main.tf
@@ -4,9 +4,9 @@
 resource "aws_subnet" "this" {
   for_each = { for s in var.subnets : s.name => s }
 
-  vpc_id            = var.vpc_id
-  cidr_block        = each.value.cidr
-  availability_zone = each.value.az
+  vpc_id                  = var.vpc_id
+  cidr_block              = each.value.cidr
+  availability_zone       = each.value.az
   map_public_ip_on_launch = each.value.type == "public" ? true : false
 
   tags = merge(var.tags, {
@@ -26,28 +26,27 @@ resource "aws_internet_gateway" "this" {
 }
 
 ############################################
-# Criando Elastic IP (Para NAT Gateway)
+# Criando Elastic IPs e NAT Gateways (Para cada AZ com Subnet Privada)
 ############################################
 resource "aws_eip" "nat" {
-  count  = length([for s in var.subnets : s if s.type == "private"]) > 0 ? 1 : 0
+  for_each = { for s in var.subnets : s.az => s if s.type == "private" }
+
   domain = "vpc"
 
-  tags = merge(var.tags, { "Name" = "${var.vpc_name}-eip-nat" })
+  tags = merge(var.tags, { "Name" = "${var.vpc_name}-eip-${each.key}" })
 }
 
-############################################
-# Criando NAT Gateway (Para Subnets Privadas)
-############################################
 resource "aws_nat_gateway" "this" {
-  count         = length([for s in var.subnets : s if s.type == "private"]) > 0 ? 1 : 0
-  allocation_id = aws_eip.nat[0].id
-  subnet_id     = aws_subnet.this["public-1a"].id  # Associando à primeira subnet pública
+  for_each = aws_eip.nat
 
-  tags = merge(var.tags, { "Name" = "${var.vpc_name}-nat" })
+  allocation_id = each.value.id
+  subnet_id     = aws_subnet.this["public-${each.key}"].id  # Associado à Subnet Pública correspondente
+
+  tags = merge(var.tags, { "Name" = "${var.vpc_name}-nat-${each.key}" })
 }
 
 ############################################
-# Criando Route Table para Subnets Públicas
+# Criando Route Table Única para Subnets Públicas
 ############################################
 resource "aws_route_table" "public" {
   count  = length([for s in var.subnets : s if s.type == "public"]) > 0 ? 1 : 0
@@ -72,26 +71,27 @@ resource "aws_route_table_association" "public" {
 }
 
 ############################################
-# Criando Route Table para Subnets Privadas
+# Criando Route Tables Individuais para Subnets Privadas (1 por AZ)
 ############################################
 resource "aws_route_table" "private" {
-  count  = length([for s in var.subnets : s if s.type == "private"]) > 0 ? 1 : 0
+  for_each = { for s in var.subnets : s.az => s if s.type == "private" }
+
   vpc_id = var.vpc_id
 
-  tags = merge(var.tags, { "Name" = "${var.vpc_name}-private-rt" })
+  tags = merge(var.tags, { "Name" = "${var.vpc_name}-private-rt-${each.key}" })
 }
 
 resource "aws_route" "private" {
-  count = length(aws_route_table.private) > 0 ? 1 : 0
+  for_each = aws_route_table.private
 
-  route_table_id         = aws_route_table.private[0].id
+  route_table_id         = each.value.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_nat_gateway.this[0].id
+  gateway_id             = aws_nat_gateway.this[each.key].id
 }
 
 resource "aws_route_table_association" "private" {
   for_each = { for s in var.subnets : s.name => s if s.type == "private" }
 
   subnet_id      = aws_subnet.this[each.key].id
-  route_table_id = aws_route_table.private[0].id
+  route_table_id = aws_route_table.private[each.value.az].id
 }
