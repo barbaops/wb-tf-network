91a13f1cf848ab4a0fbc85240de499d1e2ad416b
[!] BrunoO <38006634+barbaops@users.noreply.github.com>
2025-02-20 19:15:35 -0300

    Update variables.tf

--- subnets/variables.tf
+++ subnets/variables.tf
@@ -20,6 +20,12 @@ variable "subnets" {
   }))
 }
 
+variable "subnet_azs" {
+  description = "Lista das AZs utilizadas nas subnets"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]  # Defina as AZs que deseja usar
+}
+
 variable "tags" {
   description = "Tags adicionais para todas as subnets"
   type        = map(string)
